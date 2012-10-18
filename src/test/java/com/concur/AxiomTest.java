75c722d94b7e7894872f05196de3b7b8f6cb1c28
Scott Hoggarth <hoggarth@gmail.com>
2012-10-18 08:15:39 -0500

    Debug info.

--- src/test/java/com/concur/AxiomTest.java
+++ src/test/java/com/concur/AxiomTest.java
@@ -6,11 +6,20 @@ import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.util.AXIOMUtil;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream; 
+import java.io.OutputStreamWriter; 
+import java.nio.charset.Charset; 
+
 public class AxiomTest {
 
     @Test
     public void testCharacters()
         throws Exception {
+
+        System.out.println("Default Charset=" + Charset.defaultCharset()); 
+        System.out.println("file.encoding=" + System.getProperty("file.encoding")); 
+        System.out.println("Default Charset in Use=" + getDefaultCharSet()); 
+
         // Arrange
         String entity = "&#xFA;";
         String expected = "ú";
@@ -23,5 +32,11 @@ public class AxiomTest {
         // Assert
         Assert.assertEquals(expected, actual);
     }
+
+    private static String getDefaultCharSet() { 
+        OutputStreamWriter writer = new OutputStreamWriter(new ByteArrayOutputStream()); 
+        String enc = writer.getEncoding(); 
+        return enc; 
+    } 
 
 }
